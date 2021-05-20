ba650c69c7bfc05955d9a4fce68a61b3f3f22a05
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-19 21:29:37 -0500

    status server html

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,18 @@
 const socket = io();
+
+
+//html references
+const online  = document.querySelector('#online');
+const offline = document.querySelector('#offline');
+
+socket.on( 'connect', () => {
+    console.log('conectado');
+    offline.style.display = 'none';
+    online.style.display = '';
+} );
+
+socket.on( 'disconnect', () => {
+    console.log('desconectado');
+    offline.style.display = '';
+    online.style.display = 'none';
+} );
